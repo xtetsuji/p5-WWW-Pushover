0a405b2231bb8dac4c64ca9416f42dcb15c238a7
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2013-08-27 00:45:56 +0900

    Initial release by minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WWW::Pushover - It's new $module
+
+# SYNOPSIS
+
+    use WWW::Pushover;
+
+# DESCRIPTION
+
+WWW::Pushover is ...
+
+# LICENSE
+
+Copyright (C) OGATA Tetsuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+OGATA Tetsuji <tetsuji.ogata@gmail.com>
---
+++ lib/WWW/Pushover.pm
@@ -0,0 +1,39 @@
+package WWW::Pushover;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::Pushover - It's new $module
+
+=head1 SYNOPSIS
+
+    use WWW::Pushover;
+
+=head1 DESCRIPTION
+
+WWW::Pushover is ...
+
+=head1 LICENSE
+
+Copyright (C) OGATA Tetsuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+OGATA Tetsuji E<lt>tetsuji.ogata@gmail.comE<gt>
+
+=cut
+
