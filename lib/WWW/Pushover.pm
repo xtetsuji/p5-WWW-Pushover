6ee666e165bd9e868b20a43ed5ce972de92206a8
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2014-04-15 00:33:55 +0900

    Add POD detail documents.

--- lib/WWW/Pushover.pm
+++ lib/WWW/Pushover.pm
@@ -181,7 +181,7 @@ sub notify {
         croak "message key is required.";
     }
     else {
-        # TODO: Is message internal UTF-8 string?
+        # TODO: Is message flagged UTF-8 string?
 #        $option{message} = encode('utf-8', $option{message});
     }
     if ( DEBUG ) {
@@ -208,10 +208,32 @@ WWW::Pushover - A lightweight Pushover API warpper.
 =head1 SYNOPSIS
 
     use WWW::Pushover;
+    my $pushover = WWW::Pushover->new(
+        token => MY_PUSHOVER_TOKEN,
+        user  => MY_PUSHOVER_USER,
+    );
+    $pushover->notify(
+         message => "Hello! Pushover",
+    );
 
 =head1 DESCRIPTION
 
 WWW::Pushover is L<Pushover|http://www.pushover.net/> interface.
+
+=head1 FIRST, YOU HAVE TO CREATE API KEY
+
+This method is required Pushover API key.
+
+If you are Pushover user alredy,
+then you can create "Your Applications" on L<http://www.pushover.net/>.
+You see this "Your Application" detail, you can get "API Token/Key".
+
+If you are not Pushover user yet,
+register it. However pushover client application is charged.
+
+Now I consider whether "API Token/Key" packs together WWW::Pushover
+CPAN distribution package. In future version WWW::Pushover,
+it has its exclusive "API Token/Key" for module users convenience.
 
 =head1 METHODS
 
@@ -236,21 +258,56 @@ See following methods for detail.
     # In list context.
     my @sounds_detail = $pushover->sounds(); # output detail
 
-Output sound data. There are feteched from API server.
+Output sound names. There are feteched newest data from API server.
+
+Getting sound name is used on B<sound> method. See below.
 
 =head2 device
 
+Specify device. This is optional.
+
+Your user's device name to send the message directly to that device,
+rather than all of the user's devices
+
 =head2 title
+
+Speicfy title. This is optional.
+
+Your message's title, otherwise your app's name is used.
 
 =head2 url
 
+Specify URL. This is optional.
+
+It is specified iOS specific application launched URL schema.
+You can use it to launch other application.
+
+A supplementary URL to show with your message.
+
 =head2 url_title
+
+Specify URL's title. This is optional.
+
+A title for your supplementary URL, otherwise just the URL is shown
 
 =head2 priority
 
+Specify priority. This is optional.
+
+Send as -1 to always send as a quiet notification,
+1 to display as high-priority and bypass the user's quiet hours,
+or 2 to also require confirmation from the user.
+
 =head2 timestamp
 
+A Unix timestamp of your message's date and time to display to the user,
+rather than the time your message is received by our API.
+
 =head2 sound
+
+Specify sound to notify.
+
+See B<sounds> method for getting sound names.
 
 =head2 _json_parser
 
@@ -270,13 +327,28 @@ Execution notify operation.
 
     $pushover->notify( message => "some message" );
 
+When this method is called, WWW::Pushover sends message to specify device(s).
+
+If you want to send non-ASCII multibyte character,
+you must construct message as UTF-8.
+
+And this message's UTF-8 string is B<required as flagged (Perl internal) string> on current version's WWW::Pushover (VERSION=0.01).
+
 =head1 MOTIVATION
 
-As perl pushover API, L<WebService::Pushover> is exist.
+As perl pushover API, L<WebService::Pushover> is already exist.
 But it is too heavy, e.g. dependecy of L<Moose>, and so on.
 
-L<WWW::Pushover> concept is light interface and only core module implementation
-over Perl5.14 or it's later.
+L<WWW::Pushover> concept is light interface and
+only core module implementation over Perl5.14 or it's later.
+
+And we supports some trivial API which is not supported L<WebService::Pushover>.
+
+=head1 SEE ALSO
+
+L<WebService::Pushover>,
+
+L<Pushover REST API|https://pushover.net/api>
 
 =head1 LICENSE
 
